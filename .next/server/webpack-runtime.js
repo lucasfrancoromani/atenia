ccfdc83df388bea12aaf59412192b88b5be95ec6
lucasfrancoromani <dev@lucasromani.com>
2026-03-28 16:31:02 +0100

    feat: add admin page route and component

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("dff2ff958a100682")
+/******/ 		__webpack_require__.h = () => ("e0244a1423ade883")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d6f723c3e1c1c35c"; }
+/******/ 		__webpack_require__.h = function() { return "29c48e2650da8855"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
